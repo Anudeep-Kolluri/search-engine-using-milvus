ecc9d070c103163206793a42d7b18d4aefd3dc9b
Anudeep Kolluri <50168940+Anudeep-Kolluri@users.noreply.github.com>
2024-05-31 19:39:44 +0000

    practice code, will be deleted after this

--- main.py
+++ main.py
@@ -1,79 +1,58 @@
-from pymilvus import connections, FieldSchema, DataType, CollectionSchema, Collection
-from pymilvus import model
-from dotenv import load_dotenv
-import os
+
+#-------------------------- COMMAND LINE ARGUMENTS -----------------------
+import argparse
+
+parser = argparse.ArgumentParser(description='RAG search engine using Milvus')
+parser.add_argument('-o', '--overwrite', action='store_true', help='Overwrite the current database')
+parser.add_argument('-r', '--reload', action='store_true', help="Reload data folder and re-read the embeddings [EXPENSIVE]")
+
+args = parser.parse_args()
+
+#------------------------ HANDING VECTOR STORE ---------------------------
+
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.vector_stores.milvus import MilvusVectorStore
+from llama_index.core import StorageContext
+
+DB_NAME = "test.db"
+
+embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+# embedding_dim = embedding_model.embedding_dimension
 
 
-load_dotenv()
-
-# openai_api_key = os.getenv("OPENAI_API_KEY")
-
-
-host = "127.0.0.1"
-port = "19530"
-
-dim = 1536
-
-
-client = connections.connect(
-    host = host,
-    port = port
-)
-
-
-# create a "collection"
-def create_collection(collection_name, dim):
-
-    from pymilvus import utility
-    if utility.has_collection(collection_name):
-        utility.drop_collection(collection_name)
-
-    #define schema
-    col1 = FieldSchema(
-        name = "id",
-        dtype = DataType.VARCHAR,
-        description = "ids"
-        )
-    
-    col2 = FieldSchema(
-        name = "embedding",
-        dtype = DataType.FLOAT_VECTOR,
-        description = "embeddings",
-        dim = dim
+vector_store = MilvusVectorStore(
+    uri = DB_NAME,
+    dim = 1536,
+    overwrite=args.overwrite
     )
 
-    fields = [col1, col2]
+storage_context = StorageContext.from_defaults(vector_store = vector_store)
 
-    schema = CollectionSchema(
-        fields=fields,
-        description="RAG search"
+#---------------------- GETTING THE EMBEDDINGS ---------------------------
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, StorageContext
+
+if args.overwrite or args.reload:
+    documents = SimpleDirectoryReader("./test_data").load_data()
+    index = VectorStoreIndex.from_documents(
+        documents,
+        storage_context=storage_context
+    )
+    print("Data Loaded successfully into the Database")
+else:
+    index = VectorStoreIndex.from_vector_store(
+        vector_store=vector_store
     )
 
+#----------------------- QUERY ------------------------
+# query_engine = index.as_query_engine(llm = None)
+# response = query_engine.query("What model are you using?", llm = None)
 
-    # collection is like a table but for vectordb
-    collection = Collection(name = collection_name, schema = schema)
-
-    # collection needs index paramters
-    index_params = {
-        "metric_type" : "L2",
-        "index_type" : "IVF_FLAT",
-        "params" : {"nlist" : 128}  #leaving at default for now. but heuristic is to put sqrt(N), where n is the # of data points (rows)
-    }
-
-    collection.create_index(
-        field_name="embedding",
-        index_params = index_params
-        )
-    
-    return collection
+# print(response)
 
 
-# create embeddings of the data
-embedding_model = model.dense.OpenAIEmbeddingFunction(
-    model_name = "text-embedding-ada-002",
-)
+query = "What are transformers"
+query_engine = index.as_retriever()
+results = query_engine.retrieve(query)
 
-
-
-# table/collection is created
-collection = create_collection("RAG", dim)
+print(results)
